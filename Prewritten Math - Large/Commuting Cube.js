c026cc9712ad6b03a8369acf34813c95b07a6f38
Michael Donovan <mdono@math.mit.edu>
2012-01-06 19:39:00 -0500

    Added a cube and a triangular prism

---
+++ Prewritten Math - Large/Commuting Cube.js
@@ -0,0 +1,12 @@
+// TeXworksScript
+// Title:  Cube
+// Description: inserts a cube
+// Author:  Michael Donovan
+// Version: 0.2
+// Date: 2011-05-17
+// Script-Type: standalone
+// Context: TeXDocument
+// Shortcut:
+    
+TW.target.insertText("\\[ \\xymatrix@R=.4cm@C=.6cm{\nA  \\ar[dd]\\ar[rd]\\ar[rr] &&\nB  \\ar'[d][dd]\\ar[rd]\\\\&\nA' \\ar[dd]\\ar[rr] &&\nB' \\ar[dd] \\\\\nC  \\ar'[r][rr]\\ar[rd] &&\nD  \\ar[rd] \\\\&\nA' \\ar[rr] &&\nD'\n}\\]");
+
---
+++ Prewritten Math - Large/Commuting triangular prism.js
@@ -0,0 +1,12 @@
+// TeXworksScript
+// Title:  Triangular Prism
+// Description: inserts a triangular prism
+// Author:  Michael Donovan
+// Version: 0.2
+// Date: 2011-05-17
+// Script-Type: standalone
+// Context: TeXDocument
+// Shortcut:
+    
+TW.target.insertText("\\[ \\xymatrix@R=.4cm@C=.6cm{\nA  \\ar[dd]\\ar[rrd]\\ar[rrr]&&&\nC  \\ar[dd]\\\\&&\nB  \\ar[ru]\\ar[dd]\\\\\nA' \\ar[rrd]\\ar[rrr]&&&\nC' \\\\&&\nB' \\ar[ru]\n}\\]");
+
