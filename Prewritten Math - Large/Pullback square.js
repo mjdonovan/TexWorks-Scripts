6ceab764fb9674633aafe578cbd516902508f55b
Michael Donovan <mdono@math.mit.edu>
2012-01-06 19:55:18 -0500

    Added a pullback square.

---
+++ Prewritten Math - Large/Pullback square.js
@@ -0,0 +1,12 @@
+// TeXworksScript
+// Title: Pullback square
+// Description: inserts a cartesian square
+// Author:  Michael Donovan
+// Version: 0.2
+// Date: 2011-05-17
+// Script-Type: standalone
+// Context: TeXDocument
+// Shortcut:
+    
+TW.target.insertText("\\[\\xymatrix@C+2em@R+2em{\nQ\n\\ar@/_10pt/[ddr]_{q_1}\n\\ar@/^10pt/[drr]^{q_2}\n\\ar@{-->}[dr]|*+<3pt,3pt>{\\scriptstyle u} & & \\\\\n& P \\ar[d]_(0.4){p_1} \\ar[r]^(0.4){p_2} \n& Y \\ar[d]^{g}\\\\\n& X \\ar[r]_{f}\n& Z\n}\\]");
+
