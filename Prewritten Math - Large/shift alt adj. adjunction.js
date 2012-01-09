19e8f4e4b94b079513c045604f0ef9a965c35bdf
Michael Donovan <mdono@math.mit.edu>
2012-01-09 16:17:42 -0500

    changed the shortcut for a reversed adjunction

--- Prewritten Math - Large/shift alt adj. adjunction.js
+++ Prewritten Math - Large/shift alt adj. adjunction.js
@@ -6,7 +6,7 @@
 // Date: 
 // Script-Type: standalone
 // Context: TeXDocument
-// Shortcut: Alt+Ctrl+A,Alt+Ctrl+D,Alt+Ctrl+J
+// Shortcut: Alt+Shift+A,Alt+Shift+D,Alt+Shift+J
     
 TW.target.insertText("\\[\\xymatrix@R=.3cm@C=2cm{\n\\calA  \\ar@<-.4ex>[r]_{F}&\n\\calB  \\ar@<-.6ex>[l]_{G}\\\\\n%X \\ar@{|->}[r] & FX\\\\\n%GY             & Y \\ar@{|->}[l]\n}\\]\n");
 TW.target.selectRange(TW.target.selectionStart-115, 5)
