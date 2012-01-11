e4f48f7a71e04b266e1fbe0099be5bdd1d988ef7
Michael Donovan <mdono@math.mit.edu>
2012-01-11 14:43:12 -0500

    A macro to begin then end an environment.

---
+++ BeginEnd.js
@@ -0,0 +1,44 @@
+// TeXworksScript
+// Title:  \begin\end machine
+// Description:  Begins and ends an environment
+// Author:  Michael Donovan
+// Version: 
+// Date: 
+// Script-Type: standalone
+// Context: TeXDocument
+// Shortcut: F6
+
+var textBlock =  TW.target.selection;
+var startingposition = TW.target.selectionStart;
+var portion = TW.target.text;
+var BuildStart=portion.search("\n\n@@@ environment Builder START");
+
+var StartingText=
+"\n\n@@@ environment Builder START\nStartStartStartStartStartStart\nB: Name             @@L@@ \nEndEndEndEndEndEndEndEndEndEnd\n@@@ environment Builder END\n\n"
+
+if (BuildStart<0)
+{
+	TW.target.insertText(StartingText);
+	TW.target.selectRange(TW.target.selectionStart-1, 1);
+	TW.target.selectRange(TW.target.selectionStart-StartingText.length+1+89, 0);
+}
+else
+{
+	var BuildEnd=portion.search("@@@ environment Builder END\n\n")+29;
+	var BuilderContents = portion.substring(BuildStart,BuildEnd);
+	TW.target.selectRange(BuildStart, BuildEnd-BuildStart);
+
+	var BPos=BuilderContents.search("@@L@@ ")+6-27;
+	var ENDS=BuilderContents.search("EndEndEndEndEndEndEndEndEndEnd")+1;
+
+	BPos=BuilderContents.substring(BPos+27,ENDS-2);
+
+	var SGAP="";
+	if (BuildStart>0 && portion[BuildStart-1]!="\n")
+	{
+		SGAP="\n"
+	}
+
+	TW.target.insertText(SGAP+"\\begin{"+BPos+"}\n\n\\end{"+BPos+"}\n");
+	TW.target.selectRange(TW.target.selectionStart-BPos.length-8,0);
+}
