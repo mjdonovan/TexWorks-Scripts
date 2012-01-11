4da88b972967a2d3aafdcef237d888516a6b5a7b
Michael Donovan <mdono@math.mit.edu>
2012-01-11 14:44:03 -0500

    Added special characters to Hom.

--- Prewritten Math/hom. Hom.js
+++ Prewritten Math/hom. Hom.js
@@ -8,5 +8,5 @@
 // Context: TeXDocument
 // Shortcut: Alt+H, Alt+O, Alt+M
     
-TW.target.insertText("\\Hom()");
-TW.target.selectRange(TW.target.selectionStart-1, 0)
+TW.target.insertText("\\Hom_{\u06F7}(\u00BF,\u00BF)");
+TW.target.selectRange(TW.target.selectionStart-7, 1)
