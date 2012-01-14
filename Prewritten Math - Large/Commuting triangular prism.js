12d7b0a33068464b5cf1a1bca10f55dfaa7592ac
Michael Donovan <mdono@math.mit.edu>
2012-01-14 18:24:49 -0500

    Added more commuting prisms.

--- Prewritten Math - Large/Commuting triangular prism.js
+++ Prewritten Math - Large/Commuting triangular prism.js
@@ -8,5 +8,5 @@
 // Context: TeXDocument
 // Shortcut:
     
-TW.target.insertText("\\[ \\xymatrix@R=.4cm@C=.6cm{\nA  \\ar[dd]\\ar[rrd]\\ar[rrr]&&&\nC  \\ar[dd]\\\\&&\nB  \\ar[ru]\\ar[dd]\\\\\nA' \\ar[rrd]\\ar[rrr]&&&\nC' \\\\&&\nB' \\ar[ru]\n}\\]");
+TW.target.insertText("\n%% Start - four triangular prisms\n\\[ \\xymatrix@R=.4cm@C=.6cm{\nA  \\ar[dd]\\ar[rrd]\\ar[rrr]&&&\nC  \\ar[dd]\\\\&&\nB  \\ar[ru]\\ar[dd]\\\\\nA' \\ar[rrd]\\ar'[rr][rrr]&&&\nC' \\\\&&\nB' \\ar[ru]\n}\\]\n\\[ \\xymatrix@R=.4cm@C=.6cm{&&\nB  \\ar[rd]\\ar'[d][dd]\\\\\nA  \\ar[dd]\\ar[rru]\\ar[rrr]&&&\nC  \\ar[dd]\\\\&&\nB' \\ar[rd]\\\\\nA' \\ar[rru]\\ar[rrr]&&&\nC' \n}\\]\n\\[ \\xymatrix@R=.4cm@C=.6cm{\nA  \\ar[dd]\\ar[rd]\\ar[rrr]&&&\nC  \\ar[dd]\\\\&\nB  \\ar[rru]\\ar[dd]&\\\\\nA' \\ar[rd]\\ar'[r][rrr]&&&\nC' \\\\&\nB' \\ar[rru]&\n}\\]\n\\[ \\xymatrix@R=.4cm@C=.6cm{&\nB  \\ar[rrd]\\ar'[d][dd]&\\\\\nA  \\ar[dd]\\ar[ru]\\ar[rrr]&&&\nC  \\ar[dd]\\\\&\nB' \\ar[rrd]&\\\\\nA' \\ar[ru]\\ar[rrr]&&&\nC' \n}\\]\n%% End - four triangular prisms\n");
 
