dcaa333e5a976b1b255325efb26774dbf044bcb4
Michael Donovan <mdono@math.mit.edu>
2012-01-16 15:06:30 -0500

    Introduced single macro for (reduced) (co)homology. Deleted the old four.

---
+++ Prewritten Math/(co)homology.js
@@ -0,0 +1,12 @@
+// TeXworksScript
+// Title:  (co)homology
+// Description: One macro for (reduced) (co)homology
+// Author:  Michael Donovan
+// Version: 0.2
+// Date: 2011-05-17
+// Script-Type: standalone
+// Context: TeXDocument
+// Shortcut: Alt+Shift+H
+    
+TW.target.insertText("\u1F4A\u25BA\\widetilde{H}_{\u06F7\u25BA}^{\u06F8\u25BA}(\u25CF\u25BA;\u03E2)");
+TW.target.selectRange(TW.target.selectionStart-31, 01)
