af40fc0c553eb924a273187c25ffc6dd2755474f
Michael Donovan <mdono@math.mit.edu>
2012-01-16 15:15:57 -0500

    Wrote a script for tensor products.

---
+++ Prewritten Math/ten. Tensor.js
@@ -0,0 +1,12 @@
+// TeXworksScript
+// Title:  Tensor
+// Description: Tensor functor
+// Author:  Michael Donovan
+// Version: 0.2
+// Date: 2011-05-17
+// Script-Type: standalone
+// Context: TeXDocument
+// Shortcut: Alt+T, Alt+E, Alt+N
+    
+TW.target.insertText("{\u25CF\u25BA}\\otimes_{\u06F7\u25BA}{\u25CF}");
+TW.target.selectRange(TW.target.selectionStart-18, 1)
