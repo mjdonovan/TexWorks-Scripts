b2d6634adc3160603e319b437b9989ed55afb523
Michael Donovan <mdono@math.mit.edu>
2012-01-18 11:55:04 -0500

    Automated 'thin'.

--- Environments/thin. thin box.js
+++ Environments/thin. thin box.js
@@ -7,6 +7,23 @@
 // Script-Type: standalone
 // Context: TeXDocument
 // Shortcut: Alt+T,Alt+H,Alt+I,Alt+N
-    
-TW.target.insertText("\\makebox[0cm][l]{\\,}");
-TW.target.selectRange(TW.target.selectionStart-1, 0)
+
+if (TW.target.selection.length==0)
+{
+	TW.target.insertText("\\makebox[0cm][l\u25BA]{\u00BF}");
+	TW.target.selectRange(TW.target.selectionStart-6, 1)
+}
+else
+{
+	var textBlock =  TW.target.selection;
+	var putAtStart = "\\makebox[0cm][l]{\\,";
+	var putAtEnd = "}";
+	var INSERTME = putAtStart + textBlock + putAtEnd;
+	
+	TW.target.insertText(INSERTME);
+	TW.target.selectRange(
+		TW.target.selectionStart-putAtEnd.length-textBlock.length,
+		textBlock.length
+	)
+}
+
