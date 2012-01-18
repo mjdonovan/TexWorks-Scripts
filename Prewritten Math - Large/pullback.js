68f61b94c7a8d010fceac8473d2cbaa00446cbfa
Michael Donovan <mdono@math.mit.edu>
2012-01-18 16:57:30 -0500

    Gave pullback and pushout shortcuts.

--- Prewritten Math - Large/pullback.js
+++ Prewritten Math - Large/pullback.js
@@ -6,7 +6,7 @@
 // Date: 2011-05-17
 // Script-Type: standalone
 // Context: TeXDocument
-// Shortcut:
+// Shortcut: Alt+P,Alt+U,Alt+L,Alt+L
 
 var StartingText="\\[\\xymatrix{\n\u00BF\u25BA %Top Left\n\\ar[r]^-{\u0416\u25BA}   %Top arrow\n\\ar[d]_-{\u0416\u25BA}   %Left arrow\n\\ar@{}[rd]|{\\textup{p.b.}}&\n\u00BF\u25BA %Top Right\n\\ar[d]^-{\u0416\u25BA}\\\\ %Right arrow\n\u00BF\u25BA %Bottom Left\n\\ar[r]_-{\u0416\u25BA}&  %Bottom arrow\n\u00BF  %Bottom right\n}\\]\n";
 	
