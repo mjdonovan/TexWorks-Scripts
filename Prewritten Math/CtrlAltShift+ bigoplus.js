6310c5aa53b7880f6654f975a4f24b7d93ce91a7
Michael Donovan <mdono@math.mit.edu>
2012-01-20 17:36:28 -0500

    Added a direct sum macro.

---
+++ Prewritten Math/CtrlAltShift+ bigoplus.js
@@ -0,0 +1,12 @@
+// TeXworksScript
+// Title:  Direct sum
+// Description:  Inserts an oplus
+// Author:  Michael Donovan
+// Version: 
+// Date: 
+// Script-Type: standalone
+// Context: TeXDocument
+// Shortcut: Alt+Shift+Ctrl+=
+    
+TW.target.insertText("\\bigoplus_{\u06F7\u25BA}^{\u06F8}");
+TW.target.selectRange(TW.target.selectionStart-7, 1)
