dd5cbe178ed483a59b9298241aa88a198420c3ca
Michael Donovan <mdono@math.mit.edu>
2012-01-21 13:51:54 -0500

    Added coproduct command.

---
+++ Prewritten Math/copr. coprod.js
@@ -0,0 +1,12 @@
+// TeXworksScript
+// Title:  coproduct
+// Description:  a coproduct
+// Author:  Michael Donovan
+// Version: 
+// Date: 
+// Script-Type: standalone
+// Context: TeXDocument
+// Shortcut: Alt+C,Alt+O,Alt+P,Alt+R
+    
+TW.target.insertText("\\coprod_{\u06F7\u25BA}^{\u06F8}");
+TW.target.selectRange(TW.target.selectionStart-7, 1)
