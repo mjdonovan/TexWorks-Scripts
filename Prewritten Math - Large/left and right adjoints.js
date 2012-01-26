32fda8281d01539c6c55479157c1e7d712c786c3
Michael Donovan <mdono@math.mit.edu>
2012-01-26 16:09:42 -0500

    Added script for a pair of adjunctions.

---
+++ Prewritten Math - Large/left and right adjoints.js
@@ -0,0 +1,12 @@
+// TeXworksScript
+// Title:  Left and right adjoints
+// Description: inserts left and right adjoints
+// Author:  Michael Donovan
+// Version: 0.2
+// Date: 2011-05-17
+// Script-Type: standalone
+// Context: TeXDocument
+// Shortcut:
+    
+TW.target.insertText("\\[\\xymatrix@C=1.5cm{\n\\scrE^\\scrC\\ar@/^1.2em/[r]^-{\\Lan_K}_-{\\perp} \\ar@/_1.2em/[r]_-{\\Ran_K}^-{\\perp}&%r1c1\n\\scrE^\\scrD\\ar[l]|{K^*}%r1c2\n}\\]");
+
