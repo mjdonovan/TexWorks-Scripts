4dc637ad9b6d3fc74557e559a02f3dd68a0abb49
Michael Donovan <mdono@math.mit.edu>
2012-01-26 20:29:42 -0500

    Dash macro

---
+++ Prewritten Math/CtAlSh - DASH.js
@@ -0,0 +1,11 @@
+// TeXworksScript
+// Title:  \cdots
+// Description:  inserts \cdots
+// Author:  Michael Donovan
+// Version: 
+// Date: 
+// Script-Type: standalone
+// Context: TeXDocument
+// Shortcut: Ctrl+Alt+Shift+-
+    
+TW.target.insertText("\\DASH ");
