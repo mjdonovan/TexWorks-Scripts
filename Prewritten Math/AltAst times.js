650ffa069ffc418e0339824781843be2ce370132
Michael Donovan <mdono@math.mit.edu>
2012-01-29 22:32:38 -0500

    Added \times.

---
+++ Prewritten Math/AltAst times.js
@@ -0,0 +1,11 @@
+// TeXworksScript
+// Title:  times
+// Description:  inserts \times
+// Author:  Michael Donovan
+// Version: 
+// Date: 
+// Script-Type: standalone
+// Context: TeXDocument
+// Shortcut: Alt+*
+    
+TW.target.insertText("\\times ");
