4fb35e8678265140885e25bb548e753962528b3a
Michael Donovan <mdono@math.mit.edu>
2012-01-30 14:59:45 -0500

    Improved ldots command so that it doesn't duplicate commas.

--- Prewritten Math/ld. ldots.js
+++ Prewritten Math/ld. ldots.js
@@ -7,5 +7,16 @@
 // Script-Type: standalone
 // Context: TeXDocument
 // Shortcut: Alt+L,Alt+D
-    
-TW.target.insertText(",\\ldots,");
+
+var portion = TW.target.text;
+var SELSTART = TW.target.selectionStart;
+
+if (portion.substr(Math.max(0,SELSTART-1),1)=="," ||
+    portion.substr(Math.max(0,SELSTART-2),2)==", " )
+{
+	TW.target.insertText("\\ldots,");
+}
+else
+{
+	TW.target.insertText(",\\ldots,");
+}
