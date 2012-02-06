8169f6a4c759e249bef273d1e727eaf7db1c65cf
Michael Donovan <mdono@math.mit.edu>
2012-02-06 15:50:23 -0500

    Added a second shortcut for new items.

---
+++ Text insertions/CtlEnter. item.js
@@ -0,0 +1,16 @@
+// TeXworksScript
+// Title:  item
+// Description:  inserts \item
+// Author:  Michael Donovan
+// Version: 
+// Date: 
+// Script-Type: standalone
+// Context: TeXDocument
+// Shortcut: Ctrl+RETURN
+
+var putAtStart = "\\item ";
+if (TW.target.selectionStart!=0 && TW.target.text[TW.target.selectionStart-1]!="\n")
+{
+	putAtStart="\n"+putAtStart;
+}
+TW.target.insertText(putAtStart);
