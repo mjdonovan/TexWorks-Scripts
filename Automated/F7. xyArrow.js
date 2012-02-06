4d4ef4d2dd96820593af5267e2579e4dfc47a2b8
Michael Donovan <mdono@math.mit.edu>
2012-02-06 11:57:11 -0500

    Moved the compiler for the xymatrix builder to the arrow builder file.

--- Automated/F7. xyArrow.js
+++ Automated/F7. xyArrow.js
@@ -11,13 +11,218 @@
 var textBlock =  TW.target.selection;
 var startingposition = TW.target.selectionStart;
 var portion = TW.target.text;
-var BuildStart=portion.search("\n\n@@@ Arrow Builder START");
+var MatBuildStart=portion.search("\n\n@@@ xymatrix Builder START");
+var ArBuildStart=portion.search("\n\n@@@ Arrow Builder START");
 
 var StartingText=
 "\n\n@@@ Arrow Builder START\nStartStartStartStartStartStart\nA: Direction    @@A@@ r\u25BA\nB: Superscript  @@B@@ \u2590\u258C\u25BA\nC: Subscript    @@C@@ \u2590\u258C\u25BA\nD: Arrow type   @@D@@ \u2590\u258C\u25BA\nE: Supscr pos   @@E@@ \u2590\u258C\u25BA\nF: Subscr pos   @@F@@ \u2590\u258C\u25BA\nG: Bend         @@G@@ \u2590eg \u258C^1em\u25BA\nH: Slide        @@H@@ \u2590eg \u258C+.5ex\u25BA\nI: Break Middle @@I@@ \u2590eg \\hole\u258C\u25BA\nJ: Pass Under   @@J@@ \u2590eg\u25BA [r][rr]\u258C\u25BA\nK: Supscr label @@K@@ \u2590\u258C\u25BA\nL: Subscr label @@L@@ \u2590\u258C\u25BA\nM: Middle label @@M@@ \u2590\u258C\u25BA\nN: Tail pos     @@N@@ \u2590eg \"1\"\u258C\u25BA\nO: Head pos     @@O@@ \u2590eg\u25BA [l]\u258C\u25BA\nW: 2-cell direc @@W@@ \u2590\u258C\u25BA\nX: 2-arr label  @@X@@ \u2590eg a\u258C\u25BA\nY: omit curves? @@Y@@ \u2590\u258C\u25BA\nZ: Extra curve  @@Z@@ \u2590eg 5-9\u258C\nEndEndEndEndEndEndEndEndEndEnd\n 1     2      3    4    5    6   7   8  9    0\n->>  ^{(}->  >->  |->  -->  ..>  ~>  =  =>  none\n@@@ Arrow Builder END\n\n"
 
 //THE BIG IF STATEMENT -  
-if (BuildStart<0)
+if (MatBuildStart>-1)
+{
+	var BuildEnd=portion.search("@@@ xymatrix Builder END\n\n")+26;
+	var BuilderContents = portion.substring(MatBuildStart,BuildEnd);
+	TW.target.selectRange(MatBuildStart, BuildEnd-MatBuildStart);
+
+	var APos=BuilderContents.search("@@A@@ ")+6-27;
+	var BPos=BuilderContents.search("@@B@@ ")+6-27;
+	var CPos=BuilderContents.search("@@C@@ ")+6-27;
+	var DPos=BuilderContents.search("@@D@@ ")+6-27;
+	var EPos=BuilderContents.search("@@E@@ ")+6-27;
+	var FPos=BuilderContents.search("@@F@@ ")+6-27;
+	var GPos=BuilderContents.search("@@G@@ ")+6-27;
+	var HPos=BuilderContents.search("@@H@@ ")+6-27;
+	var IPos=BuilderContents.search("@@I@@ ")+6-27;
+	var JPos=BuilderContents.search("@@J@@ ")+6-27;
+	var KPos=BuilderContents.search("@@K@@ ")+6-27;
+	var LPos=BuilderContents.search("@@L@@ ")+6-27;
+	var ENDS=BuilderContents.search("EndEndEndEndEndEndEndEndEndEnd")+1;
+	APos=BuilderContents.substring(APos+27,BPos);
+	BPos=BuilderContents.substring(BPos+27,CPos);
+	CPos=BuilderContents.substring(CPos+27,DPos);
+	DPos=BuilderContents.substring(DPos+27,EPos);
+	EPos=BuilderContents.substring(EPos+27,FPos);
+	FPos=BuilderContents.substring(FPos+27,GPos);
+	GPos=BuilderContents.substring(GPos+27,HPos);
+	HPos=BuilderContents.substring(HPos+27,IPos);
+	IPos=BuilderContents.substring(IPos+27,JPos);
+	JPos=BuilderContents.substring(JPos+27,KPos);
+	KPos=BuilderContents.substring(KPos+27,LPos);
+	LPos=BuilderContents.substring(LPos+27,ENDS-2);
+
+	APos = APos.replace(/[\u2590\u258C\u25BA]/g,'');
+	BPos = BPos.replace(/[\u2590\u258C\u25BA]/g,'');
+	CPos = CPos.replace(/[\u2590\u258C\u25BA]/g,'');
+	DPos = DPos.replace(/[\u2590\u258C\u25BA]/g,'');
+	EPos = EPos.replace(/[\u2590\u258C\u25BA]/g,'');
+	FPos = FPos.replace(/[\u2590\u258C\u25BA]/g,'');
+	GPos = GPos.replace(/[\u2590\u258C\u25BA]/g,'');
+	HPos = HPos.replace(/[\u2590\u258C\u25BA]/g,'');
+	IPos = IPos.replace(/[\u2590\u258C\u25BA]/g,'');
+	JPos = JPos.replace(/[\u2590\u258C\u25BA]/g,'');
+	KPos = KPos.replace(/[\u2590\u258C\u25BA]/g,'');
+	LPos = LPos.replace(/[\u2590\u258C\u25BA]/g,'');
+
+	if (BPos.substring(0,2)=="eg") { BPos=""}
+	if (CPos.substring(0,2)=="eg") { CPos=""}
+	if (DPos.substring(0,2)=="eg") { DPos=""}
+	if (EPos.substring(0,2)=="eg") { EPos=""}
+	if (FPos.substring(0,2)=="eg") { FPos=""}
+	if (GPos.substring(0,2)=="eg") { GPos=""}
+	if (HPos.substring(0,2)=="eg") { HPos=""}
+	if (IPos.substring(0,2)=="eg") { IPos=""}
+	if (JPos.substring(0,2)=="eg") { JPos=""}
+	if (KPos.substring(0,2)=="eg") { KPos=""}
+	if (LPos.substring(0,2)=="eg") { LPos=""}
+
+	if (FPos=="1"||FPos=="2"||FPos=="3"||FPos=="4")
+	{
+		if (FPos=="1") {FPos="@!"}
+		if (FPos=="2") {FPos="@!0"}
+		if (FPos=="3") {FPos="@!R"}
+		if (FPos=="4") {FPos="@!C"}
+	}
+	else
+	{ FPos=""}
+
+	var SDELIM = "";
+	var EDELIM = "";
+	var DEFS = "";
+	var SETTINGS = "";
+	var THELINES = ""
+
+	if (MatBuildStart>0 && portion[MatBuildStart-1]!="\n")
+	{
+		SDELIM="\n"
+	}
+	if (BPos=="\\[")
+	{
+		SDELIM=SDELIM+"\\["
+		EDELIM="\\]"+EDELIM
+	}
+	if (BPos=="$"||BPos=="$$")
+	{
+		SDELIM=SDELIM+"$";
+		EDELIM="$"+EDELIM
+	}
+	EDELIM="}"+EDELIM;
+	if (JPos=="y" || KPos=="y" || HPos=="y")
+	{
+		if (HPos=="y")
+		{
+			SDELIM=SDELIM+"\\vcenter"
+		}
+		SDELIM=SDELIM+"{"
+		if (JPos=="y" || KPos=="y")
+		{
+			SDELIM=SDELIM+"\n";
+		}
+		EDELIM="}"+EDELIM
+		if (JPos=="y")
+		{
+			SDELIM=SDELIM+"\\def\\objectstyle{\\scriptstyle}\n"
+		}
+		if (KPos=="y")
+		{
+			SDELIM=SDELIM+"\\def\\labelstyle{\\scriptstyle}\n"
+		}
+	}
+	if (CPos.length>0)
+	{
+		SETTINGS=SETTINGS+"@="+CPos;
+	}
+	else
+	{
+		if (DPos.length>0)
+		{
+			SETTINGS=SETTINGS+"@R="+DPos;
+		}
+		if (EPos.length>0)
+		{
+			SETTINGS=SETTINGS+"@C="+EPos;
+		}
+	}
+	SETTINGS=SETTINGS+FPos;
+	if (GPos=="y")
+	{
+		SETTINGS=SETTINGS+"@1";
+	}
+	if (IPos.length>0)
+	{
+		SETTINGS=SETTINGS+"@"+IPos;
+	}
+	if (APos.length==2 && APos.match(/[1-9][1-9]/)!=null)
+	{
+		var numR = 0;
+		var numC = 0;
+		var R;
+		var C;
+		if (APos[0]=="1") {numR =1}
+		if (APos[0]=="2") {numR =2}
+		if (APos[0]=="3") {numR =3}
+		if (APos[0]=="4") {numR =4}
+		if (APos[0]=="5") {numR =5}
+		if (APos[0]=="6") {numR =6}
+		if (APos[0]=="7") {numR =7}
+		if (APos[0]=="8") {numR =8}
+		if (APos[0]=="9") {numR =9}
+
+		if (APos[1]=="1") {numC =1}
+		if (APos[1]=="2") {numC =2}
+		if (APos[1]=="3") {numC =3}
+		if (APos[1]=="4") {numC =4}
+		if (APos[1]=="5") {numC =5}
+		if (APos[1]=="6") {numC =6}
+		if (APos[1]=="7") {numC =7}
+		if (APos[1]=="8") {numC =8}
+		if (APos[1]=="9") {numC =9}
+		for (R=1;R<=numR;R++)
+		{
+			for (C=1;C<=numC;C++)
+			{
+				if (C!=1 || R!=1)
+				{
+					THELINES=THELINES+"\u2590\u258C";
+				}
+				if (C!=numC || R!=numR)
+				{
+					THELINES=THELINES+"\u25BA";
+				}
+				if (C!=numC)
+				{
+					THELINES=THELINES+"&%r"+R+"c"+C+"\n"
+				}
+				else
+				{
+					if (R!=numR)
+					{
+						THELINES=THELINES+"\\\\%r"+R+"c"+C+"\n"
+					}
+					else
+					{
+						THELINES=THELINES+"%r"+R+"c"+C+"\n"
+					}
+				}
+			}
+		}
+	}
+	if (LPos!="y")
+	{
+		var INSERTMEa = SDELIM+"\\xymatrix"+SETTINGS+"{\n";
+		var INSERTMEb = THELINES+EDELIM;
+		TW.target.insertText(SDELIM+"\\xymatrix"+SETTINGS+"{\n"+THELINES+EDELIM);	
+		TW.target.selectRange(TW.target.selectionStart-1, 1);
+		TW.target.selectRange(
+			TW.target.selectionStart-INSERTMEa.length-INSERTMEb.length+1, 1);
+		TW.target.selectRange(TW.target.selectionStart+INSERTMEa.length, 0);
+	}
+	else
+	{
+		TW.target.insertText(SETTINGS);
+		TW.target.selectRange(TW.target.selectionStart-SETTINGS.length, 0);
+	}
+}
+else if (ArBuildStart<0)
 {
 	TW.target.insertText(StartingText);
 	TW.target.selectRange(TW.target.selectionStart-1, 1);
@@ -26,8 +231,8 @@ if (BuildStart<0)
 else
 {
 	var BuildEnd=portion.search("@@@ Arrow Builder END\n\n")+23;
-	var BuilderContents = portion.substring(BuildStart,BuildEnd);
-	TW.target.selectRange(BuildStart, BuildEnd-BuildStart);
+	var BuilderContents = portion.substring(ArBuildStart,BuildEnd);
+	TW.target.selectRange(ArBuildStart, BuildEnd-ArBuildStart);
 	
 	var APos=BuilderContents.search("\nA: Direction    @@A@@ ");
 	var BPos=BuilderContents.search("\nB: Superscript  @@B@@ ");
