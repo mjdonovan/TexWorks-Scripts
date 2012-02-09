9f143003e77ae7649271a412507188885deee218
Michael Donovan <mdono@math.mit.edu>
2012-02-08 22:51:44 -0500

    added 'bullet'.

---
+++ Prewritten Math/Alt+. bullet.js
@@ -0,0 +1,11 @@
+// TeXworksScript
+// Title:  bullet
+// Description:  Inserts a bullet
+// Author:  Michael Donovan
+// Version: 
+// Date: 
+// Script-Type: standalone
+// Context: TeXDocument
+// Shortcut: Alt+.
+    
+TW.target.insertText("\\bullet ");
