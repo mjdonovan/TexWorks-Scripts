eb49cd6897bff068aaa0586c24d45610d29d4fb6
Michael Donovan <mdono@math.mit.edu>
2012-04-04 15:59:09 -0400

    Added a couple of easy commands.

---
+++ Prewritten Math/down. downarrow.js
@@ -0,0 +1,11 @@
+// TeXworksScript
+// Title:  downarrow
+// Description:  Inserts a downarrow
+// Author:  Michael Donovan
+// Version: 
+// Date: 
+// Script-Type: standalone
+// Context: TeXDocument
+// Shortcut: Alt+D,Alt+O,Alt+W,Alt+N
+    
+TW.target.insertText("\\downarrow ");
---
+++ Prewritten Math/phi. emptyset.js
@@ -0,0 +1,11 @@
+// TeXworksScript
+// Title:  emptyset
+// Description:  inserts \emptyset
+// Author:  Michael Donovan
+// Version: 
+// Date: 
+// Script-Type: standalone
+// Context: TeXDocument
+// Shortcut: Alt+p,Alt+h,Alt+i
+    
+TW.target.insertText("\\emptyset ");
